ac9baae70176f1d4cc5e0ae42afb04083f0d6281
SaiPhone <saiphone94@yandex.ru>
2021-11-07 13:53:12 +0300

    Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Введите а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("Большее {0}, меньшее {1}", a, b);
+else if (a == b)
+    Console.WriteLine("Числа a и b равны и их значение {0}", a);
+else
+    Console.WriteLine("Большее {0}, меньшее {1}", b, a);
