ec7b2cbca1471b91e7291bc0313f62610886dca6
SaiPhone <saiphone94@yandex.ru>
2021-11-07 14:18:40 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿// 4. Найти максимальное из трех чисел
+int a = 69;
+int b = 34;
+int c = 9;
+int max = a;
+if (b>max)
+    max = b;
+if(c>max)
+    max=c;
+Console.WriteLine("Максимальное число {0}", max);
