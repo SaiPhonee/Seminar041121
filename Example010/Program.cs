9b3627bb8d61ea475c3a63fe49d48d0830448631
SaiPhone <saiphone94@yandex.ru>
2021-11-07 15:03:40 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,11 @@
+﻿//9. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1000 && n > 99)
+{
+    n = n % 10;
+    Console.WriteLine("Последняя цифра {0}", n);
+}
+else
+    Console.WriteLine("Введено неверное значение");
