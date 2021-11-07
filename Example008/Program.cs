c2880183fb55b2d0d735faf6724cf4f14d6e1d72
SaiPhone <saiphone94@yandex.ru>
2021-11-07 14:48:55 +0300

    Example008

---
+++ Example008/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать числа от -N до N
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n;
+n = -n;
+Console.WriteLine(" ");
+for (int i = n; i <= a; i++)
+{
+    Console.Write(i + " ");
+}
