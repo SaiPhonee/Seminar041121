39c1d93eaa42f6fb7a6de324fe8cac3413c1e2c1
SaiPhone <saiphone94@yandex.ru>
2021-11-07 14:35:28 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,10 @@
+﻿// 5. Написать программу вычисления значения функции y=f(a)
+Console.WriteLine("Введите а");
+int x = Convert.ToInt32(Console.ReadLine());
+// int x =  4;
+void Funk (int x)
+{
+    int y = x*x  -1;
+    Console.WriteLine("f({0}) = {1}",x,y);
+}
+Funk(x);
