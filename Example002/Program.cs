dc36817e927c283c6cd855c9100a831bfd7c38b8
SaiPhone <saiphone94@yandex.ru>
2021-11-07 13:43:49 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. По двум введённым числам проверять является ли первое квадратом второго
+// int a = 5;
+//int b = 25;
+Console.WriteLine("Введите а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b == a * a)
+    Console.WriteLine("Является");
+else
+    Console.WriteLine("Не является");
