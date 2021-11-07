fe6ec13de3a12bcd69ab08f399d61670aa3ec3e1
SaiPhone <saiphone94@yandex.ru>
2021-11-07 15:09:03 +0300

    Example011

---
+++ Example011/Program.cs
@@ -0,0 +1,10 @@
+﻿//10. Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1000 && n > 99)
+{
+    n = n % 100 / 10;
+    Console.WriteLine("Вторая {0}", n);
+}
+else
+    Console.WriteLine("Введено неверное значение");
