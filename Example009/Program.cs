cb60027e4540b6d303a085ee6cc4122e6aeee474
SaiPhone <saiphone94@yandex.ru>
2021-11-07 14:57:53 +0300

    Example009

---
+++ Example009/Program.cs
@@ -0,0 +1,12 @@
+﻿//8. Показать четные числа от 1 до N
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+if (n <= 1)
+    Console.WriteLine("Введено неверное значение");
+else
+    for (int i = 2; i <= n;)
+    {
+        Console.Write(i + " ");
+        i = i + 2;
+    }
